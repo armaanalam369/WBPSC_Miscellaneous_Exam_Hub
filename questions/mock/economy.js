8b7b395d20f991fd2ba7456ca3136f0a80f2253b
Armaaaaaan <armaanalam369@gmail.com>
2026-09-19 01:28:06 +0530

    Update titles for mock economy subjects

--- questions/mock/economy.js
+++ questions/mock/economy.js
@@ -19,7 +19,7 @@
 
 addSubject("mock", "economy", [
   {
-    "title": "WBUA MISC-2024 (PH-I)",
+    "title": "WBPSC MISC-2024 (MT-1",
     "date": "27 Aug 2026",
     "questions": [
       {
@@ -98,7 +98,7 @@ addSubject("mock", "economy", [
 
 addSubject("mock", "economy", [
   {
-    "title": "WBUA MISC-2024 (PH-I) — VST-2",
+    "title": "WBPSC MISC-2024 (MT-2",
     "date": "03 Sep 2026",
     "questions": [
       {
@@ -147,7 +147,7 @@ addSubject("mock", "economy", [
 
 addSubject("mock", "economy", [
   {
-    "title": "WBUA MISC-2024 (PH-I) — VST-3",
+    "title": "WBPSC MISC-2024 (MT-3",
     "date": "10 Sep 2026",
     "questions": [
       {
@@ -196,7 +196,7 @@ addSubject("mock", "economy", [
 
 addSubject("mock", "economy", [
   {
-    "title": "WBUA MISC-2024 (PH-I) — VST-4",
+    "title": "WBPSC MISC-2024 (MT-4)",
     "date": "17 Sep 2026",
     "questions": [
       {
