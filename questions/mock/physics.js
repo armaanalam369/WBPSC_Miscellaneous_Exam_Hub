e8a30a659a318051045d12e19becc0be36c2b758
Armaaaaaan <armaanalam369@gmail.com>
2026-09-19 01:51:17 +0530

    Update mock physics titles for WBPSC MISC-2024

--- questions/mock/physics.js
+++ questions/mock/physics.js
@@ -19,7 +19,7 @@
 
 addSubject("mock", "physics", [
   {
-    "title": "WBUA MISC-2024 (PH-I)",
+    "title": "WBPSC MISC-2024 (MT-1)",
     "date": "27 Aug 2026",
     "questions": [
       {
@@ -88,7 +88,7 @@ addSubject("mock", "physics", [
 
 addSubject("mock", "physics", [
   {
-    "title": "WBUA MISC-2024 (PH-I) — VST-2",
+    "title": "WBPSC MISC-2024 (MT-2)",
     "date": "03 Sep 2026",
     "questions": [
       {
@@ -257,7 +257,7 @@ addSubject("mock", "physics", [
 
 addSubject("mock", "physics", [
   {
-    "title": "WBUA MISC-2024 (PH-I) — VST-3",
+    "title": "WBPSC MISC-2024 (MT-3)",
     "date": "10 Sep 2026",
     "questions": [
       {
@@ -326,7 +326,7 @@ addSubject("mock", "physics", [
 
 addSubject("mock", "physics", [
   {
-    "title": "WBUA MISC-2024 (PH-I) — VST-4",
+    "title": "WBPSC MISC-2024 (MT-4)",
     "date": "17 Sep 2026",
     "questions": [
       {
