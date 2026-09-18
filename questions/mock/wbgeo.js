2475cb210c02ee9b18bebf8ce91163dbda70d3bb
Armaaaaaan <armaanalam369@gmail.com>
2026-09-19 01:55:49 +0530

    Update mock exam titles for wbgeo

--- questions/mock/wbgeo.js
+++ questions/mock/wbgeo.js
@@ -19,7 +19,7 @@
 
 addSubject("mock", "wbgeo", [
   {
-    "title": "WBUA MISC-2024 (PH-I)",
+    "title": "WBPSC MISC-2024 (MT-1)",
     "date": "27 Aug 2026",
     "questions": [
       {
@@ -48,7 +48,7 @@ addSubject("mock", "wbgeo", [
 
 addSubject("mock", "wbgeo", [
   {
-    "title": "WBUA MISC-2024 (PH-I) — VST-3",
+    "title": "WBPSC MISC-2024 (MT-3)",
     "date": "10 Sep 2026",
     "questions": [
       {
@@ -67,7 +67,7 @@ addSubject("mock", "wbgeo", [
 
 addSubject("mock", "wbgeo", [
   {
-    "title": "WBUA MISC-2024 (PH-I) — VST-4",
+    "title": "WBPSC MISC-2024 (MT-4)",
     "date": "17 Sep 2026",
     "questions": [
       {
